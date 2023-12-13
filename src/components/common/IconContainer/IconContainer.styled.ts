c59f967bd44acbb9b1b2b2c566e98d6702d00013
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-12-14 00:46:53 +0200

    smth

---
+++ src/components/common/IconContainer/IconContainer.styled.ts
@@ -0,0 +1,7 @@
+/** @jsxImportSource @emotion/react */
+// import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const Container = styled.span`
+   display: block;
+`;
--- src/components/common/IconContainer/IconContainer.tsx
+++ src/components/common/IconContainer/IconContainer.tsx
@@ -1,3 +1,10 @@
-export const IconContainer: React.FC = ({ children }) => {
-   return <span>{children}</span>;
+import { Container } from "./IconContainer.styled";
+
+interface Props {
+   width: string;
+   children: React.ReactNode;
+}
+
+export const IconContainer: React.FC<Props> = ({ children, width }: Props) => {
+   return <Container>{children}</Container>;
 };
